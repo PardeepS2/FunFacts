18c5e9a025ccf9e5bac69d3263ac5d75011d30d6
PardeepS2 <Pardeemaloya@gmail.com>
2017-09-14 17:29:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gurdeep.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
