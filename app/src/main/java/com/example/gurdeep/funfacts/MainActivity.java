3bd7672ad6a0414444045733503a0e82430a4411
PardeepS2 <Pardeemaloya@gmail.com>
2017-09-15 08:44:10 -0400

    declare variables and assign values.

--- app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
+++ app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.gurdeep.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare our View Variables
+    private TextView factTextView;
+    private Button   showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign the views from the layout file to the corresponding variables
+
+        factTextView = (TextView) findViewById(R.id.factTtextView);
+        showFactButton = (Button) findViewById(R.id.ShowFactButton);
     }
 }
