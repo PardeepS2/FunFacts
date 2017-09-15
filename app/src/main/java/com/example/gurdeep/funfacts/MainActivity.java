7e0d85f306aa5fd799044cd3f59cd4f1f3b21bf5
PardeepS2 <Pardeemaloya@gmail.com>
2017-09-15 14:11:43 -0400

    Simple refactoring using new class.

--- app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
+++ app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private factbook factbook = new factbook();
     // Declare our View Variables
     private TextView factTextView;
     private Button   showFactButton;
@@ -27,23 +28,9 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-                // The button was clicked, so update the fact TextView with the new fact.
-                // Randomly select fact.
-                Random random = new Random();
-                int randomnumber = random.nextInt(facts.length);
 
-                String fact = facts [randomnumber];
+
+                String fact = factbook.getFact();
                 factTextView.setText(fact);
             }
         };
