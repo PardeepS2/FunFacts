b41ab1d4324a1c48ed44af0083f86e76bb1da48a
PardeepS2 <Pardeemaloya@gmail.com>
2017-09-15 16:49:26 -0400

    toast notification.

--- app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
+++ app/src/main/java/com/example/gurdeep/funfacts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gurdeep.funfacts;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,5 +44,6 @@ public class MainActivity extends AppCompatActivity {
         };
 
         showFactButton.setOnClickListener(listener);
+        Toast.makeText(this, "yay! our activity is created", Toast.LENGTH_SHORT).show();
     }
 }
